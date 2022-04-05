8423cef07188d50a7f56f9adfa308dde4ef9a88a
Antonio Javier Armenteros Martínez <ajam0011@red.ujaen.es>
2022-04-05 21:02:52 +0200

    Desarrollo de la situación inicial y imagen rumor.jpg añadida

--- games/media/games/tutorial/VictorJones.game.js
+++ games/media/games/tutorial/VictorJones.game.js
@@ -30,14 +30,24 @@ undum.game.slideUpSpeed = 500
 undum.game.situations = {
     comienzo: new undum.SimpleSituation(
         "<h1>Una receta para ser inmortal</h1>" +
-        "<p>Víctor Jones, un jóven que ya es casi un licenciado en ingeniería informática que sólo le faltan aprobar algunas asignaturas.</p></br></br>" +
-        "<p>Un día, yendo a una de sus clases, escucha de otros compañeros en los pasillos un <em>cierto rumor</em> que corre sobre la receta de la inmortalidad " +
-        "pero que sólo se encuentra en una zona desértica de Almería donde siempre hay mucha calima.</p>" +
-        "<p>Fascinado y curioso por la idea, al acabar sus clases de la universidad <a href='final'>vuelve a su casa.</a></p>"
+        "<p>Víctor Jones es un jóven que ya es casi un licenciado en ingeniería informática, pues sólo le faltan aprobar algunas asignaturas.</p></br></br>" +
+        "<p>Un día, yendo a una de sus clases, escucha de otros compañeros en los pasillos un <em>cierto rumor</em> que corre sobre la receta de la inmortalidad..</br></br></p>" +
+        "<img src='media/img/victorjones/rumor.jpg' width='60%' style='alignment: center'>" +
+        "<p>Compañero 1 - <em>Oye, ¿sabes que en Almería está la receta de la inmortalidad?</em></p>" +
+        "<p>Compañero 2 - <em>Pero qué dices</em></p>" +
+        "<p>Compañero 1 - <em>Que sí, que allí está la vida eterna. Te lo digo yo.</em></br></p>" +
+        "<p>Compañero 2 - <em>¿Y cómo te has enterado tú d'eso?</em></p>" +
+        "<p>Compañero 1 - <em>Pues lo escuché de un experto de inmortalidades en Youtube.</em></p>" +
+        "<p>Compañero 2 - <em>¿Pero eso tié' que estar muy escondío', no?</em></p>" +
+        "<p>Compañero 1 - <em>Según tengo entendido está en un lugar desértico donde siempre hay mucha calima. Lo llaman 'Oasis C'.</em></p>" +
+        "<p>Compañero 2 - <em>Va a ir quié' yo me sé ha'ta allí. No te creas todo lo que está por el Interné dese'.</em></p></br>" +
+
+        "<p>Víctor, fascinado y curioso por la idea de vivir eternamente, al acabar sus clases de la universidad <a href='final'>vuelve a su casa</a> para coger todo lo necesario " +
+        "y hacer un plan para llegar hasta allí</p>"
     ),
     final: new undum.SimpleSituation(
         "<h1>Todo lo que empieza, acaba</h1>" +
-        "<p>INVENTARSE EL FINAL</p>"
+        "<p>HACER LA HISTORIA DEL FINAL</p>"
     )
 };
 
